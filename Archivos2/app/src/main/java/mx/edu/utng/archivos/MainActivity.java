bf4aaafa3da8db797bdc60571adfe902b5afc9fe
fgerardo <fgerardo@users.noreply.github.com>
2017-02-13 00:32:01 -0600

    Add files via upload

---
+++ Archivos2/app/src/main/java/mx/edu/utng/archivos/MainActivity.java
@@ -0,0 +1,79 @@
+package mx.edu.utng.archivos;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    private EditText textBox;
+    static final int READ_BLOCK_SIZE = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textBox = (EditText) findViewById(R.id.txtText1);
+    }
+
+    public void onClickGuardar(View v){
+        String str = textBox.getText().toString();
+        try{
+            FileOutputStream fos = openFileOutput("textFile.txt", MODE_PRIVATE);
+            OutputStreamWriter osw = new OutputStreamWriter(fos);
+
+            // Escribimos el String en el archivo
+            osw.write(str);
+            osw.flush();
+            osw.close();
+
+            // Mostramos que se ha guardado
+            Toast.makeText(getBaseContext(), "Guardado", Toast.LENGTH_SHORT).show();
+
+            textBox.setText("");
+        }catch (IOException ex){
+            ex.printStackTrace();
+        }
+
+    }
+
+
+    public void onClickCargar(View v){
+        try{
+            FileInputStream fis = openFileInput("textFile.txt");
+            InputStreamReader isr = new InputStreamReader(fis);
+
+            char[] inputBuffer = new char[READ_BLOCK_SIZE];
+            String s = "";
+
+            int charRead;
+            while((charRead = isr.read(inputBuffer)) > 0){
+                // Convertimos los char a String
+                String readString = String.copyValueOf(inputBuffer, 0, charRead);
+                s += readString;
+
+                inputBuffer = new char[READ_BLOCK_SIZE];
+            }
+
+            // Establecemos en el EditText el texto que hemos leido
+            textBox.setText(s);
+
+            // Mostramos un Toast con el proceso completado
+            Toast.makeText(getBaseContext(), "Cargado", Toast.LENGTH_SHORT).show();
+
+            isr.close();
+        }catch (IOException ex){
+            ex.printStackTrace();
+        }
+
+    }
+
+}
